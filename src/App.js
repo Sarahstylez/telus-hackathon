d51c79a9f902efa623afe84a29b5733b02790364
Sarahstylez <sarahgee@my.capilanou.ca>
2024-05-31 00:59:49 -0700

    add other routes and checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header.js";
 import Navigation from "./components/Navigation/Navigation.jsx";
 import ThemePackList from "./components/ThemePackList/ThemePackList.jsx";
 import TestData from "./data/TestData";
+import Checkout from "./pages/CheckoutPage/Checkout.jsx";
 
 function App() {
   return (
@@ -14,7 +15,10 @@ function App() {
         <Routes>
           <Route path="/" element={<ThemePackList />} />
           <Route path="/theme-packs" element={<ThemePackList />} />
+          <Route path="/add-channels" element={<AddChannels />} />
+          <Route path="/premiums" element={<Premiums />} />
           <Route path="/test-data" element={<TestData />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </div>
     </Router>
