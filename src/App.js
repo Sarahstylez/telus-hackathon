3d1eeecb8e5c75f825b6dda6329ae7a0f7afe277
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-31 10:40:16 -0700

    price and tokens are updating

--- src/App.js
+++ src/App.js
@@ -16,101 +16,103 @@ import Checkout from "./pages/CheckoutPage/Checkout.jsx";
 // import Premiums from "./components/Premiums/Premiums.jsx";
 
 function App() {
-  const themePacks = themePacksData;
+    const themePacks = themePacksData;
 
-  const [selectedThemePackId, setSelectedThemePackId] = useState("100");
-  const [selectedChannels, setSelectedChannels] = useState([]);
-  const [shoppingCart, setShoppingCart] = useState([]);
-  const [token, setToken] = useState(0);
+    const [selectedThemePackId, setSelectedThemePackId] = useState("100");
+    const [selectedChannels, setSelectedChannels] = useState([]);
+    const [shoppingCart, setShoppingCart] = useState([]);
+    const [token, setToken] = useState(0);
+    let cost = 80;
+    useEffect(() => {}, [shoppingCart, token]);
+    // const [selectedPremium, setSelectedPremium] = useState(null);
 
-  useEffect(() => {}, [shoppingCart, token]);
-  // const [selectedPremium, setSelectedPremium] = useState(null);
+    const allChannels = themePacksData.reduce((acc, pack) => {
+        return acc.concat(pack.channels);
+    }, []);
 
-  const allChannels = themePacksData.reduce((acc, pack) => {
-    return acc.concat(pack.channels);
-  }, []);
+    // const handleConfirm = (selectedChannels) => {
+    //   setSelectedChannels(selectedChannels);
+    // };
 
-  // const handleConfirm = (selectedChannels) => {
-  //   setSelectedChannels(selectedChannels);
-  // };
-
-  const selectedThemePackData = themePacksData.find(
-    (pack) => pack.id === selectedThemePackId
-  );
-
-  function sumMonthlyCost() {
-    let sum = 0;
-    shoppingCart.map(
-      (themePackObj) => (sum += parseInt(themePackObj.monthly_cost))
+    const selectedThemePackData = themePacksData.find(
+        (pack) => pack.id === selectedThemePackId
     );
-    return sum;
-  }
 
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Navigation />
-        {`shopping cart: ${shoppingCart.length}`}
-        <br />
-        {`shopping cart price: ${sumMonthlyCost()}`}
-        <br />
-        {`tokens: ${token}`}
-        <Routes>
-          <Route
-            path="/theme-packs"
-            element={
-              <ThemePackList
-                themePacks={themePacks}
-                selectedThemePack={selectedThemePackId}
-                setSelectedThemePack={setSelectedThemePackId}
-                selectedChannels={selectedChannels}
-                setSelectedChannels={setSelectedChannels}
-                shoppingCart={shoppingCart}
-                setShoppingCart={setShoppingCart}
-                token={token}
-                setToken={setToken}
-                // onConfirm={handleConfirm}
-              />
-            }
-          />
-          {/* <Route path="/add-channels" element={<AddChannels />} />
+    function sumMonthlyCost() {
+        let sum = 0;
+        shoppingCart.map(
+            (themePackObj) => (sum += parseInt(themePackObj.monthly_cost))
+        );
+        cost += sum;
+        return sum;
+    }
+
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Navigation />
+                {`shopping cart: ${shoppingCart.length}`}
+                <br />
+                {`shopping cart price: ${sumMonthlyCost()}`}
+                <br />
+                {`tokens: ${token}`}
+                <Routes>
+                    <Route
+                        path="/theme-packs"
+                        element={
+                            <ThemePackList
+                                themePacks={themePacks}
+                                selectedThemePack={selectedThemePackId}
+                                setSelectedThemePack={setSelectedThemePackId}
+                                selectedChannels={selectedChannels}
+                                setSelectedChannels={setSelectedChannels}
+                                shoppingCart={shoppingCart}
+                                setShoppingCart={setShoppingCart}
+                                token={token}
+                                setToken={setToken}
+                                // onConfirm={handleConfirm}
+                            />
+                        }
+                    />
+                    {/* <Route path="/add-channels" element={<AddChannels />} />
           <Route path="/premiums" element={<Premiums />} /> */}
-          <Route path="/test-data" element={<TestData />} />
-          <Route
-            path="/checkout"
-            element={
-              <Checkout
-                selectedThemePack={selectedThemePackData}
-                selectedChannels={selectedChannels}
-                // selectedPremium={selectedPremium}
-              />
-            }
-          />
-          <Route
-            path="/add-channels"
-            element={
-              <AddChannels
-                selectedChannels={selectedChannels}
-                allChannels={allChannels}
-              />
-            }
-            // />
-            // <Route
-            //   path="/premiums"
-            //   element={
-            //     <Premiums
-            //       selectedPremium={selectedPremium}
-            //       setSelectedPremium={setSelectedPremium}
-            //     />
-            //   }
-          />
-        </Routes>
-        <Footer />
-        <RealFooter />
-      </div>
-    </Router>
-  );
+                    <Route path="/test-data" element={<TestData />} />
+                    <Route
+                        path="/checkout"
+                        element={
+                            <Checkout
+                                selectedThemePack={selectedThemePackData}
+                                selectedChannels={selectedChannels}
+                                // selectedPremium={selectedPremium}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/add-channels"
+                        element={
+                            <AddChannels
+                                selectedChannels={selectedChannels}
+                                allChannels={allChannels}
+                            />
+                        }
+                    />
+
+                    {/* <Route
+                        path="/premiums"
+                        element={
+                            <Premiums
+                                selectedPremium={selectedPremium}
+                                setSelectedPremium={setSelectedPremium}
+                            />
+                        }
+                    /> */}
+                </Routes>
+                <Footer cost={cost} token={token} />
+                <RealFooter />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
