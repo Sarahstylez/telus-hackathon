a837b70c14c5cd89f97c3191945e41e9f7e2c9a6
CallmeHenry <contact@henrynguyen.ca>
2024-05-30 19:42:05 -0700

    fix image issue

--- src/data/TestData.js
+++ src/data/TestData.js
@@ -1,24 +1,27 @@
 import data from "./themepacks-and-channel-data.json";
-import testIMG from "./primetime-data/AE.png";
+// import testIMG from "./primetime-data/AE.png";
 
-const testData = data[0];
+const testData = data;
 //component to test that data was entered correctly. remove this componennt on final submission
 function TestData() {
     console.log(testData);
-    console.log(testData.channels[0].featured_show.poster);
+    // console.log(testData.channels[0].featured_show.poster);
     return (
         <>
             <h1>{testData.themepack_name}</h1>
-            <img src={testIMG} />
+            {/* <img src={testIMG} /> */}
             <ul>
-                {testData.channels.map((item) => (
+                {testData[0].channels.map((item) => {
+                    return (
                     <li key={item.id}>
                         <h2>{item.name}</h2>
                         <img src={item.logo}></img>
                         {/* <p>{item.feature_show.title}</p> */}
                         {/* <img src={item.feature_show.poster}></img> */}
                     </li>
-                ))}
+                );
+            }
+            )}
             </ul>
         </>
     );
