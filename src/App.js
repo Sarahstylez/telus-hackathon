1935dd1a144c9dba4d48f42d2054f8e73a4c9366
Sarahstylez <sarahgee@my.capilanou.ca>
2024-05-30 23:15:44 -0700

    add header image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-
-import './App.scss';
-import ThemePackList from './components/ThemePackList/ThemePackList.jsx';
+import "./App.scss";
+import Header from "./header/header.js";
+import ThemePackList from "./components/ThemePackList/ThemePackList.jsx";
 import TestData from "./data/TestData";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ThemePackList />
       <TestData />
     </div>
   );
-
 }
 
 export default App;
