02489789bdc6e7899a8bb2212dde34a52aaca891
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-30 19:05:28 -0700

    json created. trouble with image pathing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.scss';
+import "./App.scss";
+import TestData from "./data/TestData";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <TestData />
+        </div>
+    );
 }
 
 export default App;
