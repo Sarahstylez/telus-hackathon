d29301714680eaa3dd35e12633890e067c46c871
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-31 09:20:58 -0700

    merged with develop

--- src/App.js
+++ src/App.js
@@ -3,49 +3,62 @@ import Header from "./components/Header/Header.js";
 import RealFooter from "./components/RealFooter/RealFooter.js";
 import ThemePackList from "./components/ThemePackList/ThemePackList.jsx";
 import TestData from "./data/TestData";
-import React, { useState } from 'react';
-import themePacksData from './data/themepacks-and-channel-data.json';
-import AddChannels from './components/AddChannels/AddChannels.jsx';
-import Navigation from './components/Navigation/Navigation.jsx';
+import React, { useState } from "react";
+import themePacksData from "./data/themepacks-and-channel-data.json";
+import AddChannels from "./components/AddChannels/AddChannels.jsx";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./pages/CheckoutPage/Checkout.jsx";
 
 function App() {
-  const [selectedThemePack, setSelectedThemePack] = useState("100");
-  const [selectedChannels, setSelectedChannels] = useState([]); 
-  
-  const allChannels = themePacksData.reduce((acc, pack) => {
-    return acc.concat(pack.channels);
-  }, []);
+    const [selectedThemePack, setSelectedThemePack] = useState("100");
+    const [selectedChannels, setSelectedChannels] = useState([]);
 
-  const handleConfirm = (selectedChannels) => { 
-    setSelectedChannels(selectedChannels);
-  };
+    const allChannels = themePacksData.reduce((acc, pack) => {
+        return acc.concat(pack.channels);
+    }, []);
 
-  return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <Routes>
-          <Route
-            path="/theme-packs"
-            element={<ThemePackList
-              themePacksData={themePacksData}
-              selectedThemePack={selectedThemePack}
-              setSelectedThemePack={setSelectedThemePack}
-              onConfirm={handleConfirm} 
-            />}
-          />
-          {/* <Route path="/add-channels" element={<AddChannels />} />
+    const handleConfirm = (selectedChannels) => {
+        setSelectedChannels(selectedChannels);
+    };
+
+    return (
+        <Router>
+            <div className="App">
+                <Navigation />
+                <Footer />
+                <Routes>
+                    <Route
+                        path="/theme-packs"
+                        element={
+                            <ThemePackList
+                                themePacksData={themePacksData}
+                                selectedThemePack={selectedThemePack}
+                                setSelectedThemePack={setSelectedThemePack}
+                                onConfirm={handleConfirm}
+                            />
+                        }
+                    />
+                    {/* <Route path="/add-channels" element={<AddChannels />} />
           <Route path="/premiums" element={<Premiums />} /> */}
-          <Route path="/test-data" element={<TestData />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/add-channels" element={<AddChannels selectedChannels={selectedChannels} allChannels={allChannels} />} />
-        </Routes>
-        <RealFooter />
-      </div>
-    </Router>
-  );
+                    <Route path="/test-data" element={<TestData />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route
+                        path="/add-channels"
+                        element={
+                            <AddChannels
+                                selectedChannels={selectedChannels}
+                                allChannels={allChannels}
+                            />
+                        }
+                    />
+                </Routes>
+
+                <RealFooter />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import "./Footer.scss";
+function Footer(props) {
+    return (
+        <>
+            <div className="footer">
+                <section className="footer__container">
+                    <div className="footer__info">
+                        <h2>Bundle total</h2>
+                        <h6>$80/mo.</h6>
+                        <h6 className="footer__discount">$80/mo.</h6>
+                    </div>
+                    <button className="footer__button">
+                        Check Availability
+                    </button>
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
