4a83e1c41c78d9bc0296f5093859e02fc7e034d1
Sarahstylez <sarahgee@my.capilanou.ca>
2024-06-03 16:32:01 -0700

    add .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "react-hooks/exhaustive-deps": [
+      "warn",
+      { additionalHooks: "sumMonthlyCost" },
+    ],
+    ...(process.env.CI ? {} : { "no-unused-vars": "warn" }),
+  },
+};
